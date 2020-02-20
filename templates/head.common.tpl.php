4600feede476b179accc8507af606107ae1b3354
Tung Pham <tung.42@gmail.com>
2020-02-20 15:08:16 +0700

    change favicon

--- templates/head.common.tpl.php
+++ templates/head.common.tpl.php
@@ -9,7 +9,7 @@
 <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $cdn_url; ?>/static/img/app-icons/apple-touch-icon-ipad<?php echo (isset($sub) && $sub != '') ? '-'.$sub : ''; ?>.png">
 <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $cdn_url; ?>/static/img/app-icons/apple-touch-icon-iphone-retina<?php echo (isset($sub) && $sub != '') ? '-'.$sub : ''; ?>.png">
 <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $cdn_url; ?>/static/img/app-icons/apple-touch-icon-ipad-retina<?php echo (isset($sub) && $sub != '') ? '-'.$sub : ''; ?>.png">
-<link rel="icon" sizes="32x32" href="<?php echo $cdn_url; ?>/static/img/favicon-32x32.png" />
+<link rel="icon" sizes="32x32" href="<?php echo $cdn_url; ?>/static/img/favicon-32x32<?php echo (isset($sub) && $sub != '') ? '-'.$sub : ''; ?>.png" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous" />
 <link href="<?php echo $cdn_url; ?>/static/css/index.css?v=78" rel="stylesheet">
 <link rel="manifest" href="<?php echo $cdn_url; ?>/manifest.webmanifest">
