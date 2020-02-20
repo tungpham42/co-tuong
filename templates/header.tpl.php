ece0cdb91233f8694b05473b4a0a84f78e89260c
Tung Pham <tung.42@gmail.com>
2020-02-20 14:59:35 +0700

    init

---
+++ templates/header.tpl.php
@@ -0,0 +1,15 @@
+<header class="site-header shadow-lg fixed-top">
+  <div class="container mx-auto">
+    <div class="row align-items-center">
+      <a class="navbar-brand mr-auto my-0" href="/vi/"><img src="<?php echo $cdn_url; ?>/static/img/app-icons/logo.png" class="xiangqi-logo" alt="xiangqi logo"><span>Cờ Tướng</span></a>
+      <div class="menu-toggle open" title="Trình đơn"></div>
+      <nav class="navbar py-0">
+        <ul class="nav navbar-nav">
+          <li class="pt-4">
+            <a class="home" href="/">Trang nhất</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
