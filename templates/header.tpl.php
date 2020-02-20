dc1e3e92dc032589551e11ef0657cc97115c4dbb
Tung Pham <tung.42@gmail.com>
2020-02-20 15:03:51 +0700

    Update header.tpl.php

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -1,7 +1,7 @@
 <header class="site-header shadow-lg fixed-top">
   <div class="container mx-auto">
     <div class="row align-items-center">
-      <a class="navbar-brand mr-auto my-0" href="/vi/"><img src="<?php echo $cdn_url; ?>/static/img/app-icons/logo.png" class="xiangqi-logo" alt="xiangqi logo"><span>Cờ Tướng</span></a>
+      <a class="navbar-brand mr-auto my-0" href="/"><img src="<?php echo $cdn_url; ?>/static/img/app-icons/logo.png" class="xiangqi-logo" alt="xiangqi logo"><span>Cờ Tướng</span></a>
       <div class="menu-toggle open" title="Trình đơn"></div>
       <nav class="navbar py-0">
         <ul class="nav navbar-nav">
